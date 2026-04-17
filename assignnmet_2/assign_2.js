75729468db979761a4af72218a91916ae8409f09
ABDULLAH <163725272+ABDULLAH-AI484@users.noreply.github.com>
2026-04-17 19:27:45 +0500

    Delete assignnmet_2 directory

--- assignnmet_2/assign_2.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Declare a variable for storing your favorite movie name with a string type annotation.
-let favoriteMovie = "Inception";
--- assignnmet_2/assign_2.ts
+++
@@ -1,2 +0,0 @@
-// Declare a variable for storing your favorite movie name with a string type annotation.
-let favoriteMovie: string = "Inception";
